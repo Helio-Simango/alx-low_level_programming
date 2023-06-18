6b9a4732828a7860de7617331127fd496621e14f
Helio-Simango <heliojanuariosimango>
2023-06-18 12:44:37 -0700

    print all the single decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/**
+* main - print all the single decimal numbers
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
