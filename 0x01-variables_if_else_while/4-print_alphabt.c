ce207b56e2cf1a115d0cd7dbfffd49567b0ecdc1
Helio-Simango <heliojanuariosimango>
2023-06-18 12:02:16 -0700

    prints the alphabet in lowercase except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase except e and q
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		if (!(i == 101) && !(i == 113))
+		{
+			putchar(i);
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
