6d9028dc5802cb242034940044e07b7756d44a9e
Helio-Simango <heliojanuariosimango>
2023-06-18 13:15:52 -0700

    prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/**
+* main - prints the lowercase alphabet in reverse
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
