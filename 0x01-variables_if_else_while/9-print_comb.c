9a847bf6bcb72469255c690e3a0da494e9b44622
Helio-Simango <heliojanuariosimango>
2023-06-18 13:47:48 -0700

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 56; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		putchar(44);
-		putchar(32);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	putchar(57);
 	putchar(10);
 
 	return (0);
