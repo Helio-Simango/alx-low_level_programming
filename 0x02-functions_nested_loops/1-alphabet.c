e3359b4e3701f7b0a8a991fe7de9df4f59a482db
Helio-Simango <heliojanuariosimango>
2023-06-21 11:02:58 -0700

    printing all alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 97; letter <= 122; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
