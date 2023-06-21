2784b944f5698840e1b66b5a9cb5dac3ebc316a7
Helio-Simango <heliojanuariosimango>
2023-06-21 11:41:42 -0700

    check if char is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - verify if char is alphabet
+ * @c: is the char to be checked
+ *
+ * Return: 1 if char is lowercase or uppercase, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
