af2d86863c776323c3ff7642c8b254871dbbd596
Helio-Simango <heliojanuariosimango>
2023-06-22 13:30:07 -0700

    building new function, to multiiplay 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplay to numbers
+* @a: is the number to be multiplayed
+* @b: is the number to be multiplayed
+* Return: the multiplication a and b
+*/
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
