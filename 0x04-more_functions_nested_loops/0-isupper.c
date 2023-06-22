8fc3e5b4482539c865d83156b9d6a4b52081ecda
Helio-Simango <heliojanuariosimango>
2023-06-22 12:44:28 -0700

    building the isupper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,16 @@
 /**
 * _isupper - check the char if is uppercase
 * @c: is the char to be checked
-* Return: 1 if char c is uppercase,otherwise 0
 *
+* Return: 1 if char c is uppercase,otherwise 0
 */
 
 int _isupper(int c)
 {
 	if (c >= 'A' || c <= 'Z')
+	{
 		return (1);
+	}
+
 	return (0);
 }
