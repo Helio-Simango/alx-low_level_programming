06d1e132da2435c678ccbf4b1ccb5037a1a0a160
Helio-Simango <heliojanuariosimango>
2023-06-26 17:52:28 -0700

    swapping pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * swap_int - Swap the values of two integers
@@ -8,10 +8,9 @@
 
 void swap_int(int *a, int *b)
 {
-	int *aux;
+	int store;
 
-	aux = *a;
+	store = *a;
 	*a = *b;
-	*b = aux;
-	return;
+	*b = store;
 }
