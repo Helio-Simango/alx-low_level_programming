2052bf7990a11f5e98aa7239a48c28132b1dbb52
Helio-Simango <heliojanuariosimango>
2023-06-26 17:35:38 -0700

    starting a pointer project

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
 * reset_to_98 - the function reset the value of the
-*               variable given using a pointer, address
+* variable given using a pointer, address
 * @n: the pointer address
 *
 * Return: reset of the variable by 98
