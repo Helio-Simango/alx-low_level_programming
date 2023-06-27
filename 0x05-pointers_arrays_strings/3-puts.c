04bbabb8983cb8979b0290ee949158235e0903a7
Helio-Simango <heliojanuariosimango>
2023-06-26 18:11:57 -0700

    a function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - description
+ * @str: Pointer to string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; (*s++ != 0); i++)
+	{
+		_putchar(*s + i);
+	}
+	_putchar('\n');
+}
