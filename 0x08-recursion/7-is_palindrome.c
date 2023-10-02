75fd6242339db302893f4ba88e9be0897f4e9554
Helio-Simango <heliojanuariosimango>
2023-10-02 06:36:02 +0000

    hot fixing

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,55 +1,47 @@
 #include "main.h"
 
-/**
- * _strlen_rec - Return the length of a string
- * @s: String
- * Return: Lengt
- */
-int _strlen_rec(char *s)
-{
-	int size;
-
-	if (*s != '\0')
-	{
-		size = _strlen_rec(s + 1);
-		return (size + 1);
-	}
-	else
-	{
-		return (0);
-	}
-}
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- * compare - Compare a string and its reverse.
- * @s: String.
- * @u: Reverse string.
- * Return: 1 if the strings are the same and 0 if not.
- */
-int compare(char *s, char *u)
-{
-	if (*s == '\0')
-		return (1);
-
-	if (*s == *u)
-		return (compare(s + 1, u - 1));
-	else
-		return (0);
-}
-
-/**
- * is_palindrome - Identify if a string is a palindrome.
- * @s: String.
- * Return: 1 if a string is a palindrome and 0 if not.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
 {
-	int size;
-	char *u = 0;
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
 
-	size = _strlen_rec(s);
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
 
-	u = s + size - 1;
-
-	return (compare(s, u));
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
