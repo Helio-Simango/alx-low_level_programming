2ddf375d45c849a7015457cca2b8e43589590f3d
Helio-Simango <heliojanuariosimango>
2024-01-23 16:26:47 +0000

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - prints the name of the file it was compiled from
-*
-* Return: Everything worked ok
-*/
-
+ * main - prints the name of the file it was
+ * compiled from
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	print("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
